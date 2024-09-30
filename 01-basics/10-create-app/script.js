72e29d482f72c4cf822d6112932e794ee5a8a450
dmotylev <dmotylev@mmtr.ru>
2024-09-30 17:59:32 +0300

    Первая задача на знакомство с Vue.

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,18 @@
-import { defineComponent, createApp } from 'vue'
+import {defineComponent, createApp} from 'vue/dist/vue.esm-browser.js'
+
+const App = defineComponent({
+  name: 'App',
+  setup() {
+  },
+
+  template: `
+    <div>Сегодня {{
+        new Date().toLocaleDateString('ru-RU', {
+          dateStyle: 'long'
+        })
+      }}
+    </div>
+  `,
+})
+
+createApp(App).mount('#app')
